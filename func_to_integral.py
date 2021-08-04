0defcbafe1eac50b129f29a44f148f2505006f9e
Shubham Gupta <shubhastro2@gmail.com>
2021-08-04 19:50:07 +0530

    Optimised Antiderivative

--- func_to_integral.py
+++ func_to_integral.py
@@ -3,34 +3,33 @@ import matplotlib.pyplot as plt
 import math
 
 def f(x):
-    return math.log(x)
+    return np.sin(x)
+
+def antif(x):
+    return -np.cos(x)
 
 def graph_from_integral(a, b,  dx=0.01):
+
     dx = dx * (1 if b>a else -1)
     n_blocks = int((b - a)/dx)
-    F = []
-    xs = []
+
+    points = []
 
     for n in range(n_blocks):
         x = a + 0
         area_till_n = []
         for _ in range(n):
             area_till_n.append(f(x)*dx)
+
             x += dx
-        F.append(sum(area_till_n))
 
-    for _ in range(n_blocks):
-        xs.append(a+dx)
-        a += dx
+        points.append((x, sum(area_till_n)))
 
-    plt.plot(xs, F)
+    xs = [point[0] for point in points]
+
+    plt.plot(xs, [antif(x) for x in xs])
+    plt.plot(xs, [point[1] for point in points])
     plt.show()
 
 
-
-
-graph_from_integral(1.1, 5)
-
-
-
-
+graph_from_integral(-5, 5)

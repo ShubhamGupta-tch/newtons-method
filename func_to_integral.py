c12672c02f93e5e0b0a96e13361fd963bac2ccb1
Shubham Gupta <shubhastro2@gmail.com>
2021-08-06 02:52:46 +0530

    Non-Elementry Integral

--- func_to_integral.py
+++ func_to_integral.py
@@ -3,12 +3,12 @@ import matplotlib.pyplot as plt
 import math
 
 def f(x):
-    return np.sin(x)
+    return (1 - (x**4))**(1/2)
 
 def antif(x):
-    return -np.cos(x)
+    return x**2
 
-def graph_from_integral(a, b,  dx=0.01):
+def graph_from_integral(a, b,  dx=0.001):
 
     dx = dx * (1 if b>a else -1)
     n_blocks = int((b - a)/dx)
@@ -23,13 +23,21 @@ def graph_from_integral(a, b,  dx=0.01):
 
             x += dx
 
+    for n in range(n_blocks):
+        x = a + 0
+        area_till_n = []
+        for _ in range(n):
+            area_till_n.append(f(x)*dx)
+
+            x += dx
+
         points.append((x, sum(area_till_n)))
 
     xs = [point[0] for point in points]
 
-    plt.plot(xs, [antif(x) for x in xs])
+    # plt.plot(xs, [antif(x) for x in xs])
     plt.plot(xs, [point[1] for point in points])
     plt.show()
 
 
-graph_from_integral(-5, 5)
+graph_from_integral(-1, 1)
